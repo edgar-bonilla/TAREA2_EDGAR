51785f2a407929a8f783cb6c84f4cb9888b77f22
edgar-bonilla <esgareliecer32@gmail.com>
2024-12-15 16:10:04 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '../router/index';
+
+const app = createApp(App);
+
+
+//app.config.globalProperties.$url = import.meta.env.VITE_API_URL;
+
+app.config.globalProperties.$url = 'http://localhost:8888';  
+
+
+app.use(router).mount('#app');
+
